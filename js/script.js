8aeee84f41c732fa0e6b7f3febeb7f6883143742
Jarek Kajko <jaruto7@gmail.com>
2018-08-23 21:36:08 +0200

    Add restart button to rewind to the beginning of carousel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 var elem = document.querySelector('.main-carousel');
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.restart-button');
+
 var flkty = new Flickity( elem, {
   	// options
 	cellAlign: 'left',
@@ -8,11 +11,18 @@ var flkty = new Flickity( elem, {
 	hash: true,
 });
 
-
 // // element argument can be a selector string
 // //   for an individual element
 // var flkty = new Flickity( '.main-carousel', {
 //   // options
 
 // });
-
+buttons = fizzyUIUtils.makeArray( buttons );
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.restart-button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
+});
