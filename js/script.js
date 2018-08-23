3c5baf2ca451800e80278a96b01f5a0ea8c8fca2
Jarek Kajko <jaruto7@gmail.com>
2018-08-23 19:13:12 +0200

    Hide dots under carousel by modify script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+  	// options
+	cellAlign: 'left',
+	//disable dots
+	pageDots: false
+
 });
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
   // options
+
 });
