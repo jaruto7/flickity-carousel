e6ddf9cb072e659f8ed5d94c77e2e83d390cf1fd
Jarek Kajko <jaruto7@gmail.com>
2018-08-24 23:54:34 +0200

    Create loop in JS to output all object items in index.html file

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ var flkty = new Flickity( elem, {
 	// Enable hash behavior
 	hash: true,
 });
+
 // Use progress bar scroll
 flkty.on( 'scroll', function( progress ) {
 	progress = Math.max( 0, Math.min( 1, progress ) );
@@ -18,6 +19,7 @@ flkty.on( 'scroll', function( progress ) {
   });
 // Use button to restart
 buttons = fizzyUIUtils.makeArray( buttons );
+
 buttonGroup.addEventListener( 'click', function( event ) {
   // filter for button clicks
   if ( !matchesSelector( event.target, '.restart-button' ) ) {
@@ -25,7 +27,18 @@ buttonGroup.addEventListener( 'click', function( event ) {
   }
   var index = buttons.indexOf( event.target );
   flkty.select( index );
-
-  
-  
 });
+
+var page-wrapper = document.getElementById('page-wrapper');
+
+Mustache.parse(page-wrapper);
+
+var listItems = '';
+
+for(var i = 0; i < myObject.length; i++) {
+  listItems = listItems + Mustache.render(page-wrapper, myObject[i]);
+}
+
+var allItems = Mustache.render(page-wrapper, {title: listItems}, {image: listItems}, {description: listItems});
+
+page-wrapper.insertAdjacentHTML('beforeend', allItems);
