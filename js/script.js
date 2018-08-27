1a1d16895601aa78bbdfe5d52af3da8fb66fe359
Jarek Kajko <jaruto7@gmail.com>
2018-08-27 22:20:59 +0200

    Prepare to: fix Google Map not properly loaded

--- js/script.js
+++ js/script.js
@@ -21,6 +21,17 @@ var fullProductList = Mustache.render(tmplMain, {carousel: listItems});
 // console.log(listItems);
 result.innerHTML = fullProductList;
 
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: {coords}, map: map});
+}
+
 // Use flickity to create carousel and add some options
 var flkty = new Flickity( elem, {
   	// options
